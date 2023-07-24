d007903117ffced5d8e66faa28ec86d29eeddbdf
Ericdid <ericdidom@gmail.com>
2023-07-24 17:26:33 +0200

    add.roll-dice

---
+++ dadi/main.js
@@ -0,0 +1,15 @@
+// lanciamo due dadi a 6 facce
+
+function player1() {
+  return 1 + Math.floor(Math.random() * 12);
+}
+
+for (let i = 1; i < 2; i++) console.log(player1());
+
+// il computer lancia due dadi a 6 facce
+
+function player2() {
+  return 1 + Math.floor(Math.random() * 12);
+}
+
+for (let i = 1; i < 2; i++) console.log(player2());
